099377fa520f9cbfef7ccc517c905bc68a6b41ca
fantou.jerome@gmail.com <Milare64!>
2020-02-15 14:59:14 -0500

    add project configServer and folderGit for file properties

---
+++ configServer/src/test/java/com/netbrains/configServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netbrains.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
